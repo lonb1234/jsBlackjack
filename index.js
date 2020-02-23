a38b2d680b530a55e2ccc283cb672fa80c604305
Lon <lonbouweriks@gmail.com>
2020-02-23 15:59:22 +0100

    stand button has limited functionality

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ var dealer = 0
 var player = 0
 var randomNumberC = 0
 var randomNumberD = 0
-pScore=0
-cardcounter=0
+pScore = 0
+DScore=0
+cardcounter = 0
+cardcounterD= 0
+randomD=0
+
+var deal=parseInt(document.querySelector(".scoreD").innerHTML)
+var player=document.querySelector(".scoreP").innerHTML
+
 
 function rollcard() {
   // // randomnumberc to 0
@@ -31,6 +38,23 @@ function rollcard() {
   // document.querySelector(".scoreP").innerHTML=randomNumberD
 }
 
+function rollcardD() {
+  // // randomnumberc to 0
+  // randomNumberC=randomNumberC*0
+
+  randomNumberA = Math.floor((Math.random() * 4) + 1)
+  randomNumberB = Math.floor((Math.random() * 13) + 1)
+  // randomnumber to 10 for +10 cards
+  if (randomNumberB > 10) {
+    randomD = randomNumberB - (randomNumberB - 10)
+  } else {
+    randomD = randomNumberB
+  }
+
+
+  // document.querySelector(".scoreP").innerHTML=randomNumberD
+}
+
 
 // startup of page, random cards assigned to both player and dealer, skipping the first card of the dealer and the player because it will be the first drawn card later.
 for (i = 1; i < 12; i++) {
@@ -42,17 +66,27 @@ for (i = 1; i < 12; i++) {
 
 // adding scores , function bc often needing it.
 
-function addScoreP(){
+function addScoreP() {
   if (randomNumberB > 10) {
     randomNumberD = randomNumberB - (randomNumberB - 10)
   } else {
     randomNumberD = randomNumberB
   }
-  Pscore=Pscore+randomNumberD
+  Pscore = Pscore + randomNumberD
 
   document.querySelector(".scoreP ").innerHTML = Pscore
 }
 
+function addScoreD() {
+  if (randomNumberB > 10) {
+    randomD = randomNumberB - (randomNumberB - 10)
+  } else {
+    randomD = randomNumberB
+  }
+  DScore = DScore + randomD
+
+  document.querySelector(".scoreD ").innerHTML = DScore
+}
 
 
 
@@ -61,46 +95,49 @@ function addScoreP(){
 
 // new game button , fully functional, random backcover on unturned cards, picks a random card for the first card of the dealer and the first 2 of the player.
 
- function newgamez() {
+function newgamez() {
+cardcounterD=0
+cardcounterD++
+  cardcounter = 0
+  cardcounter++
+  randomNumberC = 0
+  money = 250
+  bet = 0
+  Pscore = 0
+  DScore=0
+  document.querySelector(".total").innerHTML = "Total money: $250"
+  document.querySelector(".current").innerHTML = "Current bet: $0"
 
-   cardcounter=0
-   cardcounter++
-   randomNumberC = 0
-   money = 250
-   bet = 0
-   Pscore=0
-   document.querySelector(".total").innerHTML = "Total money: $250"
-   document.querySelector(".current").innerHTML = "Current bet: $0"
+  // set loop
+  for (i = 0; i < 12; i++) {
+    if (i != 0 && i != 6 && i != 7) {
+      randomNumber1 = Math.floor((Math.random() * 4) + 1)
+      document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/s" + randomNumber1 + ".png");
+    } else if (i < 1) {
+      rollcard()
+      document.querySelector(".scoreD").innerHTML = randomNumberC
+      if (randomNumberB > 9) {
+        document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/" + randomNumberA + "-" + randomNumberB + ".png");
+      } else {
+        document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/" + randomNumberA + "-0" + randomNumberB + ".png");
+      }
+    } else {
+      rollcard()
+      addScoreP()
+      if (randomNumberB > 9) {
+        document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/" + randomNumberA + "-" + randomNumberB + ".png");
+      } else {
+        document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/" + randomNumberA + "-0" + randomNumberB + ".png");
+      }
+    }
+  }
+}
 
-   // set loop
-   for (i = 0; i < 12; i++) {
-     if (i != 0 && i != 6 && i != 7) {
-       randomNumber1 = Math.floor((Math.random() * 4) + 1)
-       document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/s" + randomNumber1 + ".png");
-     } else if (i < 1) {
-       rollcard()
-       document.querySelector(".scoreD").innerHTML = randomNumberC
-       if (randomNumberB > 9) {
-         document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/" + randomNumberA + "-" + randomNumberB + ".png");
-       } else {
-         document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/" + randomNumberA + "-0" + randomNumberB + ".png");
-       }
-     } else {
-       rollcard()
-       addScoreP()
-       if (randomNumberB > 9) {
-         document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/" + randomNumberA + "-" + randomNumberB + ".png");
-       } else {
-         document.querySelectorAll(".card")[i].setAttribute("src", "images/PNG/" + randomNumberA + "-0" + randomNumberB + ".png");
-       }
-     }
-   }}
-
-   function NewGame(){
+function NewGame() {
   // confirm start game
   var r = confirm("Are you certain you want to start a new game?");
   if (r == true) {
-newgamez()
+    newgamez()
     // reset randon numberc
 
   }
@@ -129,48 +166,53 @@ function bets() {
 };
 
 
-function drawhit(){
-  if (randomNumberB > 9) {
-    document.querySelector(".cardp"+(cardcounter+1)).setAttribute("src", "images/PNG/" + randomNumberA + "-" + randomNumberB + ".png");
-  addScoreP()
 
-
-  } else {
-    document.querySelector(".cardp"+(cardcounter+1)).setAttribute("src", "images/PNG/" + randomNumberA + "-0" + randomNumberB + ".png");
-addScoreP()
-}}
 
 
 // Hit
-function hit(){
+function hit() {
+  rollcard()
+  if (cardcounter <= 4 && cardcounter > 0) {
+    cardcounter++
+    if (randomNumberB > 9) {
+      document.querySelector(".cardp" + (cardcounter + 1)).setAttribute("src", "images/PNG/" + randomNumberA + "-" + randomNumberB + ".png");
+      addScoreP()
+
+
+    } else {
+      document.querySelector(".cardp" + (cardcounter + 1)).setAttribute("src", "images/PNG/" + randomNumberA + "-0" + randomNumberB + ".png");
+      addScoreP()
+    }
+    if (Pscore > 21) {
+      alert("You lose!")
+      newgamez()
+    }
+  }
+  if (cardcounter == 5) {
+    stand()
+  }
+}
+
+
+
+
+
+function stand() {
+  for (cardcounterD; DScore<17 && cardcounterD<7; cardcounterD++){
+  addScoreD()
     rollcard()
-  if (cardcounter==1){
-cardcounter ++
-
-drawhit()
-if(Pscore>21)
-{alert("You lose!")
-newgamez()}}
-else if (cardcounter==2){
-cardcounter ++
-drawhit()
-if(Pscore>21)
-{alert("You lose!")
-newgamez()}}
-else if (cardcounter==3){
-cardcounter ++
-drawhit()
-if(Pscore>21)
-{alert("You lose!")
-newgamez()}}
-else if (cardcounter==4){
-cardcounter ++
-drawhit()
-if(Pscore>21)
-{alert("You lose!")
-newgamez()}}}
+  if (randomNumberB > 9) {
+    document.querySelector(".card" + (cardcounterD)).setAttribute("src", "images/PNG/" + randomNumberA + "-" + randomNumberB + ".png");
 
 
+
+  }
+  else {
+    document.querySelector(".card" + (cardcounterD)).setAttribute("src", "images/PNG/" + randomNumberA + "-0" + randomNumberB + ".png");
+
+  }
+}
+}
 
 
 //stand - draw cards loop till 21+ if 21+ win, compare player score w dealer score if below
